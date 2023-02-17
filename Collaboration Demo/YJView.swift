91a0c404eba06a51c7a0a37b85f5ff34eb4d6534
YJ Soon <yjsoon@tinkertanker.com>
2023-02-17 14:26:17 +0800

    Updated YJ view to be colourful

--- Collaboration Demo/YJView.swift
+++ Collaboration Demo/YJView.swift
@@ -9,7 +9,14 @@ import SwiftUI
 
 struct YJView: View {
     var body: some View {
-        Text("This is YJ's page")
+        ZStack {
+            LinearGradient(
+                colors: [.red, .green, .blue],
+                startPoint: .topLeading,
+                endPoint: .bottomTrailing)
+            .edgesIgnoringSafeArea(.all)
+            Text("This is YJ's page")
+        }
     }
 }
 
